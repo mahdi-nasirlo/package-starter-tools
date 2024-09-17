797927e64e2ac34f72ae5c7cf9ace65a7dcfb3f0
mahdi <m.nwordpress1223@gmail.com>
2024-09-17 13:46:04 +0330

    delete user data

--- index.ts
+++ index.ts
@@ -1,9 +1,7 @@
-import getUser from "./src/utils/get-user";
 import OidcAuthProvider from "./src/providers/oidc-auth-provider";
 import { useAuth } from "oidc-react";
 
 export {
   OidcAuthProvider as AuthProvider,
   useAuth,
-  getUser,
 };
