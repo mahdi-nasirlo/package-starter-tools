d5bb6cdd33c6c9b1cbd73bab103bca64e37828b4
mahdi <m.nwordpress1223@gmail.com>
2024-09-23 16:01:23 +0330

    version 1.1.15

--- src/providers/protected-page-provider.tsx
+++ src/providers/protected-page-provider.tsx
@@ -21,11 +21,46 @@ const ProtectedPageProvider = ({ children, loading, axiosInstance }: TProps) => 
 
     const singOut = useSignOutRedirect();
 
-    const [hubConnection, setHubConnection] = useState()
+    const [hubConnection, setHubConnection] = useState<signalR.HubConnection>();
 
     const { userData, signIn, isLoading } = useAuth();
 
     useEffect(() => {
+
+        const startConnection = () => {
+            const connection = new signalR.HubConnectionBuilder()
+                .withUrl(`${process.env.NEXT_PUBLIC_OPIC_AUTHORITY}/notificationHub`)
+                .build();
+
+            connection
+                .start()
+                .then(() => {
+                    console.log("Connection started");
+
+                    connection.on("ReceiveMessage", async (data: any) => {
+                        await onEvent?.({
+                            data,
+                            signOut: true,
+                            type: "error",
+                            onClose: singOut,
+                        });
+                    });
+
+                    connection.on("KillUser", async (data: any) => {
+                        await onEvent?.({
+                            data,
+                            signOut: true,
+                            type: "error",
+                            onClose: singOut,
+                        });
+                    });
+                })
+                .catch((err: Error) =>
+                    console.error("Error while starting connection: " + err)
+                );
+
+            setHubConnection(connection);
+        };
 
     }, []);
 
